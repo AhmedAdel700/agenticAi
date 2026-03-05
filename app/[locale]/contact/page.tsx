5add6b0ca9d26eff4e0273a1b8b93122b34850e0
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2026-03-05 21:23:43 +0200

    fix: add contact page inside [locale] to restore /contact route

---
+++ app/[locale]/contact/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout/Layout";
+import Contact from "@/components/sections/home2/Contact";
+
+export default function ContactPage() {
+    return (
+        <Layout headerStyle={1} footerStyle={1} breadcrumbTitle="Contact Us">
+            <Contact/>
+        </Layout>
+    )
+}
